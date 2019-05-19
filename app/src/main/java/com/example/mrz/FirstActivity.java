fa08e16a72794375244a895bc91f443304f13d8f
zhoulei <729169254@qq.com>
2019-05-19 23:13:29 +0800

    创建第一个活动 使用Toast 使用Menu

---
+++ app/src/main/java/com/example/mrz/FirstActivity.java
@@ -0,0 +1,52 @@
+package com.example.mrz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        //通过getMenuInflater()获取MenuInflater对象，
+        //inflate()构造menu 第一个参数指通过那个资源文件创建菜单
+        // 第二个参数指定菜单将加入哪一个Menu对象 选取传入得到menu参数
+        return true;
+        //return true显示
+        //false 无法显示
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(FirstActivity.this, "你点了添加", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(FirstActivity.this, "你点了删除", Toast.LENGTH_LONG).show();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        button = (Button) findViewById(R.id.button_toast);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this, "这是一个Toast", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/mrz/MainActivity.java
+++ app/src/main/java/com/example/mrz/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.mrz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +16,8 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"点了",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, FirstActivity.class);
+                startActivity(intent);
             }
         });
     }
