88a5d1f14d5ed568024509859a6567dcaf9c13fa
zhoulei <729169254@qq.com>
2019-05-19 22:17:07 +0800

    建立项目

---
+++ app/src/main/java/com/example/mrz/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.mrz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button_1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"点了",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
